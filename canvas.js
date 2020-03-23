a6b3b6dddf44cc15ea7146f45d4a19eb4a518a10
Kendra <kmodrunia@ucdavis.edu>
2020-03-23 15:40:49 -0700

    added rect

--- canvas.js
+++ canvas.js
@@ -1,3 +1,12 @@
 // console.log('hello')
 
 let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// within the variable called ctx, we have access to all these drawing methods
+ctx = canvas.getContext('2d');
+
+// (top-left,top-right,width, heigth)
+ctx.fillRect(100,100, 100, 100)
+ctx.fillRect(200,300, 100, 100)
